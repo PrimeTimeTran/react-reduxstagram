58290f69c68f8104bbccf42268e4cb8e69b0756a
Loi Tran <lvt15@my.fsu.edu>
2018-01-19 00:58:42 +0700

    Completed basic functionality

--- client/components/single.js
+++ client/components/single.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Photo from './photo'
+
+import Comments from './comments'
 
 const Single = React.createClass({
   render() {
+    const { postId } = this.props.params
+    const i = this.props.posts.findIndex((post) => post.code === postId);
+    const post = this.props.posts[i];
+    const postComments = this.props.comments[postId] || [];
+
     return (
       <div className='photo-grid'>
-      I'm the Single
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props}/>
       </div>
     )
   }
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,10 +1,31 @@
-function comments(state = [], action) {
-  switch (action.type) {
+function postComments(state =[], action) {
+  switch(action.type) {
     case 'ADD_COMMENT':
-
+      return [...state, {
+        user: action.author,
+        text: action.comment
+      }];
+    case 'REMOVE_COMMENT':
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1)
+      ]
     default:
-      return state
+      return state;
   }
+  return state;
+}
+
+function comments(state = [], action) {
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // take current state
+      ...state,
+      // overwrite this post with new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+  }
+  return state
 }
 
 export default comments
